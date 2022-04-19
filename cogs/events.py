769cdafa70f5eb86383caebb32498fd32a5e8034
pennacap <64080101+pennacap@users.noreply.github.com>
2022-04-19 16:38:50 +0530

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,6 +1,6 @@
 from discord import *
 from discord.ext.commands import *
-
+from requests import get
 class Events(Cog):
     def __init__(self, client):
         self.client = client
@@ -22,6 +22,14 @@ class Events(Cog):
         await message.add_reaction("👋")
 
         await member.add_roles(role)
+
+    @Cog.listener()
+    async def on_interaction(self, interaction):
+        if interaction.custom_id and interaction.message:
+            if interaction.custom_id == 'cat_next':
+                return await interaction.message.edit(content=get("https://api.thecatapi.com/v1/images/search").json()["url"])
+            elif interaction.custom_id == 'dog_next':
+                return await interaction.message.edit(content=get("https://dog.ceo/api/breeds/image/random").json()["message"])
 
 def setup(client):
     client.add_cog(Events(client))
